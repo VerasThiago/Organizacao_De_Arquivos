76287f92137026ee97ddd1646111f581155cf2e9
Thiago Veras <thiago.veras.machado@gmail.com>
2018-03-26 13:21:31 -0300

    Add files via upload

---
+++ lista1.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+#include <map>
+
+using namespace std;
+
+void menu(){ // Escrever alguma coisa para introduzir
+		printf("\n");
+
+
+}
+
+
+int main(){
+	int n;
+	string file;
+	menu();
+	cout << "Digite a quantidade de arquivos que deseja concatenar" << endl;
+	cin >> n;
+	vector<string> files
+	for(int i = 1; i <= n; i++){
+		cout << "Digite o nome do arquivo " << i << ":";
+
+
+	}
+}
