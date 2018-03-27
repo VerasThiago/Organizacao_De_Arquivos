db3635c72c90c7c104871e5f54610ffcaabfec70
VerasUnB <thiago.wortnnes@gmail.com>
2018-03-27 02:44:32 +0000

    open_all Function Working

--- lista1.cpp
+++ lista1.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <map>
 
+
 using namespace std;
 
 void menu(){ // Escrever alguma coisa para introduzir
@@ -13,17 +14,50 @@ void menu(){ // Escrever alguma coisa para introduzir
 
 }
 
+bool open_all(vector<string> files){
+	fstream arquivo;
+	for(auto file : files){
+		arquivo.open(file);
+		if(!arquivo){
+			cout << "Falha no arquivo " << file << endl;
+			return false;
+		}
+		cout << "Abriu o " << file << endl;
+		arquivo << "Penis" << endl;
+		arquivo.close();
+	}
+	return true;
+}
+
+/*void concatenate(vector<string> files){
+    std::fstream fa = std::fstream(file1, std::ios::out | std::ios::app);
+    std::fstream fb = std::fstream(file2, std::ios::in)
+
+    std::string line;
+    while( std::getline(fb, line)){
+        fa << line << "\n";
+    }
+   
+
+    return "Concatenation successful.\n";
+}*/
 
 int main(){
-	int n;
+	vector<string> files;
 	string file;
+	int n;
 	menu();
+
 	cout << "Digite a quantidade de arquivos que deseja concatenar" << endl;
 	cin >> n;
-	vector<string> files
 	for(int i = 1; i <= n; i++){
 		cout << "Digite o nome do arquivo " << i << ":";
-
-
+		cin >> file;
+		files.push_back(file + ".txt");
 	}
+	if(open_all(files)){
+		//concatenate(files);
+	}
+	//else
+
 }
